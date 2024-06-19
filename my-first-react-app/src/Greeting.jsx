f880031000f0649c0187f629faae4ca47c2966ed
tetracup <alexsklbus@gmail.com>
2024-06-19 10:29:55 +0200

    added rule for missing props

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -3,4 +3,48 @@ function Boo()
     return <img style={{borderRadius : 50}} width = "500" src="chic.png"></img>;
 }
 
+export function ListTest()
+{
+    const furniture = ["Chair", "Table", "Stool", "Wardrobe"]; 
+    const furnitureList = furniture.map( (object) => <li key = {object}>{object}Object</li>);
+    return (
+        <div>
+            <h1> Kinds of furniture </h1>
+            <ul>
+                {furnitureList}
+            </ul>
+        </div>
+    );
+}
+
+/* Setup structure of element */ 
+function ListItem(props)
+{
+    return <li> {props.fruit}</li>
+}
+
+/* Setup structure of list*/
+
+function List(props)
+{
+    return (
+        <ul>
+            {props.fruits.map((fruit) => {
+                return <ListItem key = {fruit} fruit = {fruit} /> 
+            })}
+        </ul>
+    );
+}
+
+export function ListTest2()
+{
+    const fruits = ["Orange", "Raspberry", "Strawberry", "Banana"]; 
+    return (
+        <div>
+            <h1>Fruits: </h1>
+            <List fruits = {fruits} />
+        </div>
+    );
+}
+
 export default Boo;
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Boo from './Greeting.jsx'
+/*import App from './App.jsx'*/
+import { ListTest } from './Greeting.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Boo />
+    <ListTest />
   </React.StrictMode>,
 )
