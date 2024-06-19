69b2c947d4f7536ddeb08c693eccd9850fedc807
tetracup <alexsklbus@gmail.com>
2024-06-19 16:28:27 +0200

    buttonList

--- my-first-react-app/src/Greeting.jsx
+++ my-first-react-app/src/Greeting.jsx
@@ -1,3 +1,5 @@
+import "./Greeting.css"
+import { v4 as uuid } from 'uuid'
 function Boo()
 {
     return <img style={{borderRadius : 50}} width = "500" src="chic.png"></img>;
@@ -30,7 +32,38 @@ function List(props)
     return (
         <ul>
             {props.fruits.map((fruit) => {
-                return <ListItem key = {fruit} fruit = {fruit} /> 
+                return fruit.startsWith("B") && <ListItem key = {fruit} fruit = {fruit} /> 
+            })}
+        </ul>
+    );
+}
+
+function Button({text,color,fontSize}) {
+    const buttonStyle = {
+        color: color, 
+        fontSize: fontSize + 'px'
+    };
+
+    return (
+        <li>
+            <button style = {buttonStyle}>{text}</button>
+        </li>
+    )
+        
+}
+
+Button.defaultProps = {
+    text: "Click Me!",
+    color: "blue",
+    fontSize: 12
+}
+
+function ButtonList(props)
+{
+    return (
+        <ul>
+            {props.buttonTexts.map((textObj) => {
+                return <Button key = {textObj.id} text = {textObj.text} /> 
             })}
         </ul>
     );
@@ -40,10 +73,29 @@ export function ListTest2()
 {
     const fruits = ["Orange", "Raspberry", "Strawberry", "Banana"]; 
     return (
-        <div>
-            <h1>Fruits: </h1>
-            <List fruits = {fruits} />
-        </div>
+        <>
+            <div>
+                <h1>Fruits: </h1>
+                <List fruits = {fruits} />
+            </div>
+        </>
+    );
+}
+
+export function ListTest3()
+{
+    const buttonTexts = [
+        { text: "Orange", id: uuid() },
+        { text: "Raspberry", id: uuid() },
+        { text: "Strawberry", id: uuid() },
+      ];
+    return (
+        <>
+            <div>
+                <h1> Fruit Buttons: </h1>
+                <ButtonList buttonTexts = {buttonTexts} />
+            </div>
+        </>
     );
 }
 
--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-/*import App from './App.jsx'*/
-import { ListTest } from './Greeting.jsx'
+import { ListTest3 } from './Greeting.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListTest />
+    <ListTest3 />
   </React.StrictMode>,
 )
