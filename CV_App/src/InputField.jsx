2943e4041e132fc8a8a1e9e8a9b52f6caaedd888
tetracup <alexsklbus@gmail.com>
2024-07-03 17:57:17 +0200

    finished mvp, added points to achievements

--- CV_App/src/InputField.jsx
+++ CV_App/src/InputField.jsx
@@ -1,40 +1,21 @@
 
 
-function InputField ({text, _type = "text", formInputs, fID, count, isMultiple = false}) {
+function InputField ({text, _type = "text", formInputs, fID}) {
 
     fID = fID.split('.');
 
     formInputs[fID[0]] == null ? formInputs[fID[0]] = {} : null; 
-    if(isMultiple)
-    {
-        formInputs[fID[0]][fID[1]] == null ? formInputs[fID[0]][fID[1]] = {} : null; 
-    }
 
     const handleChange = (e) => {
         formInputs[fID[0]][fID[1]] = e.target.value
     }
-    
-    const inputs = [];
-    if(isMultiple){
-        for (let i = 1; i <= count; i++) {
-            inputs.push(
-                <input  type = {_type} onChange = {(e) => handleChange(e)} defaultValue = {formInputs[fID[0]][fID[1]]} />
-            );
-        }
-    }
-    
-
-    
 
     return (
         <>
-            <div className = "inputField">
+            <div className = "inputField" >
                 <p> {text} </p> 
                 {/*<input type = {_type} onChange = {handleChange} defaultValue = {formInputs[fID[0], fID[1]]}/> */}
                 <input  type = {_type} onChange = {(e) => handleChange(e)} defaultValue = {formInputs[fID[0]][fID[1]]} />
-
-                
-
             </div>
         </>
     )
--- CV_App/src/MultipleInputField.jsx
+++ CV_App/src/MultipleInputField.jsx
@@ -1,19 +1,53 @@
 import './App.css'
 import {useState} from 'react'
-import InputField from './InputField.jsx'
 import Button from './Button.jsx'
 
 
 function MultipleInputField ({text, formInputs, fID}) {
-    const [inputFieldsCount, setInputFieldsCount] = useState(1)
+    fID = fID.split('.')
+    const [inputFieldsCount, setInputFieldsCount] = useState(localStorage.getItem(fID[0] + fID[1]) === null ? 1 : parseInt(window.localStorage.getItem(fID[0] + fID[1])))
+    
     
     const handleAdd = () => {
         setInputFieldsCount(inputFieldsCount + 1)
     }
+
+    const handleRemove = () => {
+        setInputFieldsCount(inputFieldsCount - 1)
+        delete formInputs[fID[0]][fID[1]][inputFieldsCount]
+    }
+
+    formInputs[fID[0]] == null ? formInputs[fID[0]] = {} : null; 
+    formInputs[fID[0]][fID[1]] == null ? formInputs[fID[0]][fID[1]] = {} : null; 
+    
+    const handleChange = (e, ind) => {
+        formInputs[fID[0]][fID[1]][ind] = e.target.value
+    }
+
+    const inputList = []
+
+    for(let i = 1; i <= inputFieldsCount; i++){
+        inputList.push(i)
+    }
+
+    window.localStorage.setItem(fID[0] + fID[1], inputFieldsCount)
+
+    
     return (
         <div>
-            <InputField text = {text} formInputs = {formInputs} fID = {fID} count = {inputFieldsCount} isMultiple = {true}/>
-            <Button text = "Add" clickFunc = {handleAdd} />
+            {/* <InputField text = {text} formInputs = {formInputs} fID = {fID} count = {inputFieldsCount} isMultiple = {true}/> */}
+            <div className = "inputField" >
+                <p> {text} </p>
+                {inputList.map((count) => (
+                    <input style = {{marginBottom: "10px"}} key = {count} defaultValue = {formInputs[fID[0]][fID[1]][count]} onChange = {(e) => {handleChange(e, count)}}/>
+                ))}
+                <div>
+                    <Button text = "Add" clickFunc = {handleAdd} />
+                    {inputFieldsCount > 1 ? <Button text = "Remove" clickFunc = {handleRemove} /> : null}
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
