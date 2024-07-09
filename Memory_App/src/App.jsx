1e51dd5e45299ff56def5cdc3eb57fcccf7bd2f8
tetracup <alexsklbus@gmail.com>
2024-07-09 15:28:43 +0200

    made first memory game mvp
    added highscore, score, error handling on api usage

--- Memory_App/src/App.jsx
+++ Memory_App/src/App.jsx
@@ -6,17 +6,25 @@ import {useEffect, useState}from 'react'
 
 function App() {
 
-  const [art, setArt] = useState([])
+  const [art, setArt] = useState({images: [], score: 0})
   const [search, setSearch] = useState("van gogh")
-  const selectedItems = []
-
+  let searchInput = ""
+  let highscore = 0
+  if(!localStorage.getItem('score'))
+  {
+    localStorage.setItem('score', 0)
+  }
+  else {
+    highscore = localStorage.getItem('score')
+  }
+  console.log(art)
   useEffect(() => {
     {/* Sleeping Beauties: Reawakening Fashion to test less than 10 */}
       
     const fetchIDs = async () => {
       try
       {
-        const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isPublicDomain=true&q=' + search)
+        const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=' + search)
         const data = await response.json() 
         return(data.objectIDs)
       } catch(err) 
@@ -24,14 +32,29 @@ function App() {
         console.log(err)
       }
     }
+    function checkURL(arr, url)
+    {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i][1] == url) {
+            return true
+        } 
+      }
+      return false
+    }
 
     const loopImages = async (obj, images) => {
       try{
         let id = JSON.stringify(obj)
         const response = await fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects/' + id)
-        const data = await response.json() 
-        if(data.isPublicDomain)
-          images.push([id, data.primaryImage])
+        if (!response.ok) { // <-- check response OK here
+          throw new Error('ID faulty');
+        }
+        const data = await response.json()
+        const urlLink = data.primaryImage
+        if(urlLink != "" && !checkURL(images, urlLink))
+        {
+          images.push([id, data.primaryImage, false])
+        }
       } catch (err) {
         console.log(err)
       }
@@ -49,8 +72,8 @@ function App() {
           await loopImages(IDs[i], images)
           i++
         }
-        
-        setArt(images)
+        const newArt = {...art, images: images, score: 0} 
+        setArt(newArt)
       } catch (err) {
         console.log(err.message);
       }
@@ -65,48 +88,67 @@ function App() {
     
   }, [search]);
 
-  let searchInput = ""
   const handleInput = (text) => {
     searchInput = text
   }
+
   const handleSearch = () => {
     setSearch(searchInput)
   }
-  
-  const handleArtClick = () => {
-    let indexes = Array.from(art.keys())
+
+  const handleArtClick = (ky) => {
+    let newScore = 0
+    let indexes = Array.from(art.images.keys())
     let randomised = []
-    for(let i = 0; i < art.length; i++)
+    for(let i = 0; i < art.images.length; i++)
     {
-      let ind = Math.floor(Math.random() * indexes.length)
-      
+      let ind = Math.floor(Math.random() * indexes.length)  
       randomised.push(indexes[ind])
       indexes.splice(ind, 1)
     }
 
     let newImages = []
+
     for(let i = 0; i < randomised.length; i++)
     {
-      newImages.push(art[randomised[i]])
+      let newItem = art.images[randomised[i]]
+      if(newItem[0] == ky)
+      {
+        if(newItem[2] != true)
+        {
+          newScore = art.score + 1
+          newItem[2] = true
+          if(newScore > highscore)
+            localStorage.setItem('score', newScore)
+        }
+      } 
+      newImages.push(newItem)
     }
+    const finalImages = newScore == 0 ? newImages.map((obj) => {
+      let newObj = obj; 
+      newObj[2] = false;
+      return newObj
+    }) : newImages
 
-    setArt(newImages)
+    
+    const newArt = {...art, images: finalImages, score: newScore} 
+    setArt(newArt)
   }
 
   return (
     <>
     <div className = "all">
       <div className = "header">
-        Search
+        <h1>Score: {art.score}, High Score: {localStorage.getItem('score')}</h1>
+        <h1>Search</h1>
         <input onChange = {(e) => {handleInput(e.target.value)}} type = "text"/>
         <button onClick = {handleSearch}>
           Search
         </button>
       </div>
       <div className = "main">
-        
-        {art.map((entry) => (
-          <Card key = {entry[0]} url = {entry[1]} onClick = {handleArtClick}/>
+        {art.images.map((entry) => (
+          <Card key = {entry[0]} url = {entry[1]} onClick = {() => {handleArtClick(entry[0])}}/>
         ))}
       </div>
       <div className = "footer">
